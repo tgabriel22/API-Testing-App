0ce173bef5330f9eb920a633cc787213de6f18ce
tgabriel22 <thierrygabriel87@gmail.com>
2024-10-30 12:59:05 -0400

    Implement local Editing and Deletion

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
 import { observer } from "mobx-react-lite";
 import { movieStore } from "../../mobx/movieStore";
 import { useInfiniteScroll } from "../../hooks/useInfiniteScroll";
@@ -8,6 +9,22 @@ const MovieList: React.FC = observer(() => {
   useEffect(() => {
     movieStore.loadMovies();
   }, []);
+  const [editingMovieId, setEditingMovieId] = useState<number | null>(null);
+  const [newTitle, setNewTitle] = useState<string>("");
+
+  const handleEditClick = (movieId: number, currentTitle: string) => {
+    setEditingMovieId(movieId);
+    setNewTitle(currentTitle);
+  };
+
+  const handleSaveClick = (movieId: number) => {
+    movieStore.editMovie(movieId, newTitle);
+    setEditingMovieId(null);
+  };
+
+  const handleDeleteClick = (movieId: number) => {
+    movieStore.deleteMovie(movieId);
+  };
 
   useInfiniteScroll();
 
@@ -15,12 +32,32 @@ const MovieList: React.FC = observer(() => {
     <div className="movie-list">
       {movieStore.movies.map((movie) => (
         <div key={movie.id} className="movie-card">
-          <h3>{movie.title}</h3>
-          <p>Release Date: {movie.release_date}</p>
-          <img
-            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-            alt={movie.title}
-          />
+          {editingMovieId === movie.id ? (
+            <>
+              <input
+                type="text"
+                value={newTitle}
+                onChange={(e) => setNewTitle(e.target.value)}
+              />
+              <button onClick={() => handleSaveClick(movie.id)}>Save</button>
+              <button onClick={() => setEditingMovieId(null)}>Cancel</button>
+            </>
+          ) : (
+            <>
+              <h3>{movie.title}</h3>
+              <p>Release Date: {movie.release_date}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <button onClick={() => handleEditClick(movie.id, movie.title)}>
+                Edit
+              </button>
+              <button onClick={() => handleDeleteClick(movie.id)}>
+                Delete
+              </button>
+            </>
+          )}
         </div>
       ))}
       {movieStore.isLoading && <p>Loading more movies...</p>}
--- src/mobx/movieStore.ts
+++ src/mobx/movieStore.ts
@@ -1,8 +1,14 @@
 import { makeAutoObservable } from "mobx";
 import { fetchMovies } from "../assets/services/api";
 
+interface Movie {
+  id: number;
+  title: string;
+  release_date: string;
+  poster_path: string;
+}
 class MovieStore {
-  movies: any[] = [];
+  movies: Movie[] = [];
   currentPage: number = 1;
   isLoading: boolean = false;
 
@@ -14,9 +20,22 @@ class MovieStore {
   async loadMovies() {
     this.isLoading = true;
     const newMovies = await fetchMovies(this.currentPage);
+    // console.log("Fetched movies again:", newMovies);
     this.movies = [...this.movies, ...newMovies];
     this.currentPage++;
     this.isLoading = false;
+  }
+  // Edit a movie by its ID
+  editMovie(id: number, newTitle: string) {
+    const movie = this.movies.find((movie) => movie.id === id);
+    if (movie) {
+      movie.title = newTitle;
+    }
+  }
+
+  // Delete a movie by its ID
+  deleteMovie(id: number) {
+    this.movies = this.movies.filter((movie) => movie.id !== id);
   }
 }
 
